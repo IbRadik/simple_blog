860fd34cbf12a9d4cd9d568a0eb2ff7db49a60c1
IbRadik <radikibragimow.tr@gmail.com>
2019-04-03 17:24:08 +0300

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # get 'welcome/index'
-  #
-  # resources :articles
-  #
-  # root 'welcome#index'
+  get 'welcome/index'
+
   resources :articles do
     resources :comments
   end
+
+  root 'welcome#index'
 end
