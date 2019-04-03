5e923d89354ff6cd49c43e8ec93608841dfbdefd
IbRadik <radikibragimow.tr@gmail.com>
2019-04-03 15:42:12 +0300

    blog v1

---
+++ test/models/article_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+
+  test 'valid article' do
+    article =  Article.new(title: 'Title', text: 'Body text')
+    assert article.valid?
+  end
+end
---
+++ test/models/comment_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  setup do
+    @article = articles(:one)
+  end
+
+  test 'valid comment' do
+    comment =  Comment.new(commenter: 'Alex', body: 'Body text', article: @article)
+    assert comment.valid?
+  end
+
+  test 'invalid comment without article' do
+    comment =  Comment.new(commenter: 'Alex', body: 'Body text')
+    assert comment.invalid?
+  end
+end
