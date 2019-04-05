576c3411dac1df60f725b47d4942ab949b43fa46
IbRadik <radikibragimow.tr@gmail.com>
2019-04-05 18:15:57 +0300

    Add api for creating articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v2 do
+      get 'articles/create'
+    end
+  end
   get 'welcome/index'
 
   resources :articles do
     resources :comments
+  end
+
+  namespace :api do
+    namespace :v2 do
+      resources :articles
+    end
   end
 
   root 'welcome#index'
