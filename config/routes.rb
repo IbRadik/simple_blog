4ed2907e622c27c3f1e6ece40dc6c005b6ce1e39
IbRadik <radikibragimow.tr@gmail.com>
2019-04-08 09:54:33 +0300

    cleaning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v2 do
-      get 'articles/create'
-    end
-  end
   get 'welcome/index'
 
   resources :articles do
